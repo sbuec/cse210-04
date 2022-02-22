5405489dd694bc89a9de83871a621a3e2fb070f2
Steelheart96 <steelheart696@gmail.com>
2022-02-22 15:15:43 -0800

    Fixed comment about variables.

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
+import pyray as pr
+
 class Player:
 
-    def __init__(self, window_width, window_height, texture) -> None:
-        '''You need These 4 varibales in order to work with my classes'''
+    def __init__(self, window_width, window_height, texture: pr.Texture) -> None:
+        '''You need These 4 variables in order to work with my classes'''
         self.win_width = window_width
         self.win_height = window_height
         self.texture = texture
