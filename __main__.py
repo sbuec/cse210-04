88da39f634d9545a2cdee4b135d4667ca0d6987f
Steelheart96 <steelheart696@gmail.com>
2022-02-21 18:12:46 -0800

    Adding My Offline Work - Created Asteroid class - Created Rock Class - Made main function

--- __main__.py
+++ __main__.py
@@ -1,5 +1,28 @@
-def main():
-    pass
+import pyray as pr
+import rock as Rk
 
-if __name__ == '__main__':
+def main():
+    HEIGHT = 450
+    WIDTH = 800
+
+    pr.init_window(WIDTH, HEIGHT, "Hello Pyray")
+    pr.set_target_fps(60)
+
+    rock_texture = Rk.Rock.load_texture('O', 12, pr.WHITE)
+    Rk.Rock.load_asteroid(200, WIDTH, HEIGHT, rock_texture)
+
+
+    while not pr.window_should_close():
+
+        pr.begin_drawing()
+
+        pr.clear_background(pr.BLACK)
+
+        Rk.Rock.draw_asteriod()
+
+        pr.end_drawing()
+
+    pr.close_window()
+
+if __name__ == "__main__":
     main()
