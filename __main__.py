64db1040344a5c5325cef2e26c9388cbfd74ffaa
Steven Bue <skeeve11@gmail.com>
2022-02-21 23:30:58 -0600

    Updated main and keyboard_services

--- __main__.py
+++ __main__.py
@@ -1,29 +1,62 @@
 import pyray as pr
 import rock as Rk
 import utils
-import input
+import services.keyboard_service
+import services.video_service
+#import input
+
+
+
+'''
+Variables that will be used throughout the program
+'''
+HEIGHT = 450
+WIDTH = 800
+FPS = 60
+CAPTION = "Hello Pyray"
 
 
 def main():
-    HEIGHT = 450
-    WIDTH = 800
 
-    pr.init_window(WIDTH, HEIGHT, "Hello Pyray")
-    pr.set_target_fps(60)
+    '''
+    Creating objects and settings that will be used 
+    '''
+    pr.init_window(WIDTH, HEIGHT, CAPTION)
+    pr.set_target_fps(FPS)
 
     rock_texture = Rk.Rock.load_texture('O', 12, pr.WHITE)
     Rk.Rock.load_asteroid(200, WIDTH, HEIGHT, rock_texture)
 
+    #creating instance of keyboard service
+    ks = services.keyboard_service.KeyboardService()
 
     while not pr.window_should_close():
-
+        '''
+        Things to do while window is open and game is running
+        *Display output
+        *Receive input
+        *Calculate automatic changes to the output
+        '''
+        
+        #Printing output
         pr.begin_drawing()
-
         pr.clear_background(pr.BLACK)
-
         Rk.Rock.draw_asteriod()
+        
+        #checking for input
+        #currently an example to show that input_direction is working
+        direction = ks.input_direction()
+        if direction == 1:
+            pr.draw_text("Working", 50, 50, 20, pr.WHITE)
+
+
+
 
         pr.end_drawing()
+
+
+
+
 
     pr.close_window()
 
