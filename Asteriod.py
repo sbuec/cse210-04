f802d6e2a3109ee29ef4c8a95ff94ada7b4d8b31
Steelheart96 <steelheart696@gmail.com>
2022-02-21 20:31:23 -0800

    I Finished My Classes! - I Added Gem class - I consolidated Asteroid Class - I added Asteroid Timer - I added Collision Detection - I added NEEDED Player Variables

--- Asteriod.py
+++ Asteriod.py
@@ -1,16 +1,7 @@
 import pyray as pr
 from random import randint
 
-class Asteriod:
-
-    def __init__(self, window_width: int, window_height: int, texture: pr.Texture) -> None:
-        self.win_width = window_width
-        self.win_height = window_height
-        self.texture = texture
-        self.set_timer()
-
-    def set_timer(self) -> None:
-        self.timer = randint(0, 10)*60
+class Actor:
 
     @staticmethod
     def create_image(character, font_size, color) -> pr.Image:
@@ -29,10 +20,36 @@ class Asteriod:
             [pos_x, pos_y],
             pr.WHITE
         )
+
+class Asteriod(Actor):
+
+    def __init__(self, window_width: int, window_height: int, texture: pr.Texture) -> None:
+        self.win_width = window_width
+        self.win_height = window_height
+        self.texture = texture
+        self.set_pos()
+        self.set_timer()
+
+    def draw(self) -> None:
+        self.draw_rectangle(self.texture, self.pos_x, self.pos_y)
+
+    def set_timer(self) -> None:
+        self.timer = randint(0, 10)*60
+
+    def set_pos(self) -> None:
+        self.pos_x = randint(0, self.win_width - self.texture.width)
+        self.pos_y = 0
     
     @staticmethod
     def update_actor(actor) -> None:
         actor.pos_y += 1
+    
+    @staticmethod
+    def check_collision(asteroid, player) -> bool:
+        return pr.check_collision_recs(
+                [asteroid.pos_x, asteroid.pos_y,asteroid.texture.width, asteroid.texture.height],
+                [player.pos_x, player.pos_y, player.texture.width, player.texture.height]
+            )
     
     @classmethod
     def load_asteroid(cls, amount, win_width, win_height, texture) -> None:
@@ -40,11 +57,12 @@ class Asteriod:
             cls(win_width, win_height, texture)
     
     @classmethod
-    def draw_asteriod(cls) -> None:
+    def draw_asteroid(cls, player) -> None:
         cls.timer_watch()
-        for asteroid in cls.on_screen:
+        for asteroid in cls._on_screen:
+            cls.collisions_watch(asteroid, player)
             if asteroid.pos_y >= asteroid.win_height - asteroid.texture.height:
-                cls.on_screen.remove(asteroid)
+                cls._on_screen.remove(asteroid)
                 asteroid.set_timer()
             else:
                 cls.update_actor(asteroid)
@@ -52,13 +70,19 @@ class Asteriod:
     
     @classmethod
     def timer_watch(cls) -> None:
-        for asteroid in cls.all_created:
+        for asteroid in cls._all_created:
 
-            if asteroid.timer <= 0 and asteroid not in cls.on_screen:
+            if asteroid.timer <= 0 and asteroid not in cls._on_screen:
                 asteroid.set_pos()
-                cls.on_screen.append(asteroid)
+                cls._on_screen.append(asteroid)
 
-            if asteroid.timer > 0 and asteroid in cls.on_screen:
-                cls.on_screen.remove(asteroid)
+            if asteroid.timer > 0 and asteroid in cls._on_screen:
+                cls._on_screen.remove(asteroid)
             elif asteroid.timer > 0:
                 asteroid.timer -= 1
+    
+    @classmethod
+    def collisions_watch(cls, asteroid, player) -> None:
+        if cls.check_collision(asteroid, player):
+            cls._on_screen.remove(asteroid)
+            player.points += asteroid.points
--- __main__.py
+++ __main__.py
@@ -1,15 +1,26 @@
 import pyray as pr
-import rock as Rk
+import rock as rk
+import gem as gm
+import player as pl
 
 def main():
     HEIGHT = 450
     WIDTH = 800
 
+    GEM_AMOUNT = 20
+    ROCK_AMOUNT = 150
+
     pr.init_window(WIDTH, HEIGHT, "Hello Pyray")
     pr.set_target_fps(60)
 
-    rock_texture = Rk.Rock.load_texture('O', 12, pr.WHITE)
-    Rk.Rock.load_asteroid(200, WIDTH, HEIGHT, rock_texture)
+    player_texture = pl.Player.load_texture('#', 1, pr.WHITE)
+    player = pl.Player(WIDTH, HEIGHT, player_texture)
+
+    rock_texture = rk.Rock.load_texture('O', 12, pr.WHITE)
+    rk.Rock.load_asteroid(ROCK_AMOUNT, WIDTH, HEIGHT, rock_texture)
+
+    gem_texture = gm.Gem.load_texture('*', 12, pr.GREEN)
+    gm.Gem.load_asteroid(GEM_AMOUNT, WIDTH, HEIGHT, gem_texture)
 
 
     while not pr.window_should_close():
@@ -18,7 +29,10 @@ def main():
 
         pr.clear_background(pr.BLACK)
 
-        Rk.Rock.draw_asteriod()
+        player.draw_player()
+
+        rk.Rock.draw_asteroid(player)
+        gm.Gem.draw_asteroid(player)
 
         pr.end_drawing()
 
