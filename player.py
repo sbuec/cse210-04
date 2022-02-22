fe124eb3d99823978c0486fe63a374d49c6be0d0
Steelheart96 <steelheart696@gmail.com>
2022-02-21 20:26:45 -0800

    I Finished My Classes! - I Added Gem class - I consolidated Asteroid Class - I added Asteroid Timer - I added Collision Detection - I added NEEDED Player Variables

--- player.py
+++ player.py
@@ -1,2 +1,10 @@
-class player:
-    pass
+from Asteriod import Actor
+
+class Player(Actor):
+
+    def __init__(self, window_width, window_height, texture) -> None:
+        '''You need These 4 varibales in order to work with my classes'''
+        self.win_width = window_width
+        self.win_height = window_height
+        self.texture = texture
+        self.points = 0
--- rock.py
+++ rock.py
@@ -1,22 +1,13 @@
-from random import randint
 from Asteriod import Asteriod
 from pyray import Texture
 
 class Rock(Asteriod):
 
-    on_screen = []
-    all_created = []
+    _on_screen = []
+    _all_created = []
 
     def __init__(self, window_width: int, window_height: int, texture: Texture) -> None:
         super().__init__(window_width, window_height, texture)
         self.points = -1
-        self.set_pos()
-        Rock.on_screen.append(self)
-        Rock.all_created.append(self)
-
-    def draw(self) -> None:
-        self.draw_rectangle(self.texture, self.pos_x, self.pos_y)
-
-    def set_pos(self) -> None:
-        self.pos_x = randint(0, self.win_width - self.texture.width)
-        self.pos_y = 0
+        Rock._on_screen.append(self)
+        Rock._all_created.append(self)
